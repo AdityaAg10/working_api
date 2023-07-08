4731e969e8fb631c2cedee20829a28c3ad2cab61
AdityaAg10 <120501050+AdityaAg10@users.noreply.github.com>
2023-07-08 18:49:36 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,139 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, unused_import
-
-import 'package:api_app/DeleteData.dart';
-import 'package:api_app/PostData.dart';
 import 'package:flutter/material.dart';
+
 import 'GetData.dart';
-import 'package:http/http.dart' as http;
+import 'PostData.dart';
 import 'PutData.dart';
+import 'DeleteData.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
+      theme: ThemeData(
+        primaryColor: Color(0xFF2D3142), // Set primary color
+        hintColor: Color(0xFFEF8354), // Set accent color
+        scaffoldBackgroundColor: Color(0xFFEDF2F4), // Set scaffold background color
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: Color(0xFFEF8354), // Set button color
+            padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueAccent,
       appBar: AppBar(
-        title: Text("Working API app"),
-      ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsetsDirectional.symmetric(horizontal: 100, vertical: 120),
-          child: Column(
-            children: [
-              MaterialButton(
-                child: Text(
-                  'Get',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                color: Colors.purpleAccent,
-                textColor: Colors.white,
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => GetData()));
-                },
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              MaterialButton(
-                child: Text(
-                  'Post',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                color: Colors.purpleAccent,
-                textColor: Colors.white,
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => PostData()));
-                },
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              MaterialButton(
-                child: Text(
-                  'Put',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                color: Colors.purpleAccent,
-                textColor: Colors.white,
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => PutData()));
-                },
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              MaterialButton(
-                child: Text(
-                  'Delete',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                color: Colors.purpleAccent,
-                textColor: Colors.white,
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => DeleteData()));
-                },
-              ),
-            ],
+        title: Text(
+          "Working API App",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
           ),
+        ),
+        backgroundColor: Theme.of(context).primaryColor,
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(40.0),
+            topRight: Radius.circular(40.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => GetData()),
+                );
+              },
+              child: Text(
+                'Get',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PostData()),
+                );
+              },
+              child: Text(
+                'Post',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PutData()),
+                );
+              },
+              child: Text(
+                'Put',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DeleteData()),
+                );
+              },
+              child: Text(
+                'Delete',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );

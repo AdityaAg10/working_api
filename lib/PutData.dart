3d989fec9581f35146c602c6341b498cccf6fabf
AdityaAg10 <120501050+AdityaAg10@users.noreply.github.com>
2023-07-08 18:50:31 +0530

    Update PutData.dart

--- lib/PutData.dart
+++ lib/PutData.dart
@@ -1,12 +1,9 @@
-// ignore_for_file: prefer_const_constructors, unused_import
-
 import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class PutData extends StatefulWidget {
-  const PutData({super.key});
+  const PutData({Key? key});
 
   @override
   State<PutData> createState() => _PutDataState();
@@ -14,32 +11,93 @@ class PutData extends StatefulWidget {
 
 class _PutDataState extends State<PutData> {
   final url = "https://dummy.restapiexample.com/api/v1/update/21/";
+  String putResponse = '';
 
-  void PutData() async {
+  Future<void> putData() async {
     try {
-      final response = await put(Uri.parse("https://dummy.restapiexample.com/api/v1/update/21/"), body: {
-        "name": "test",
-        "salary": "123",
-        "age": "23"
-      });
+      final response = await http.put(
+        Uri.parse(url),
+        body: {
+          "name": "test",
+          "salary": "123",
+          "age": "23"
+        },
+      );
 
-      print(response.body);
-    } catch (er) {}
+      setState(() {
+        putResponse = response.body;
+      });
+    } catch (e) {
+      print('Error occurred while updating data: $e');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purpleAccent,
       appBar: AppBar(
-        backgroundColor: Colors.purple,
-        title: Text("Put"),
+        title: Text(
+          "Put",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Theme.of(context).primaryColor,
+        centerTitle: true,
+        elevation: 0,
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-        child: ElevatedButton(
-          onPressed: PutData,
-          child: Text("Put Data"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: ElevatedButton(
+                onPressed: putData,
+                child: const Text("Put Data"),
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 12,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Card(
+              elevation: 2,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Put Response:',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      putResponse,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
